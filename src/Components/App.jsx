673859f7e2795a899031cb220216172bdc5dd1a2
Badal Kumar Behera <surajbadal69@gmail.com>
2023-06-15 01:20:58 +0530

    add file for note making app

---
+++ src/Components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../Notes";
+
+
+
+function App() {
+    return (
+        <div>
+            <Header />
+            {notes.map((createNote) => {
+                return (
+                    <Note
+                        key={createNote.key}
+                        name={createNote.title}
+                        content={createNote.content}
+                    />
+                )
+
+            })}
+            <Footer />
+        </div>
+    )
+}
+
+export default App;
